3b63ed6a5806d30cf932f1771ebba7a1766d4c91
wesley perez <wesley.m.perez@gmail.com>
2019-12-25 05:43:55 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -33,7 +33,7 @@ end
 
 def matrix_lookup(matrix, row, column)
      matrix[row][column]
-     puts matrix
+     p matrix
 
  # Given any matrix (array of arrays), a row index and a column index,
   # Return the matrix's content at that row and and column
